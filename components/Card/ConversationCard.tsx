4c7b3ca093df19d317f6401f0f56325d7689b082
Maximilian Maksutovic <maksutovic@gmail.com>
2024-07-10 01:30:40 -0700

    Added summarized component design

--- components/Card/ConversationCard.tsx
+++ components/Card/ConversationCard.tsx
@@ -9,7 +9,9 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Button } from '@/components/ui/button'
 import ProcessingDialog from '@/components/Dialogue/ProcessingDialog'
 import { mockProcessConversation } from '@/services/mockProcessingService'
-import { ClipboardIcon, TrashIcon, ChevronRightIcon } from '@/components/ui/icons'
+import { ClipboardIcon, TrashIcon } from '@/components/ui/icons'
+import { ArrowRightIcon } from "@radix-ui/react-icons"
+import { Badge } from "@/components/ui/badge";
 
 interface ConversationCardProps {
   conversation: ConversationData
@@ -32,6 +34,10 @@ const ConversationCard: React.FC<ConversationCardProps> = ({ conversation, onUpd
     }
   }
 
+  const handleOnViewTranscript = async () => {
+
+  }
+
   return (
     <motion.div initial={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0.8 }} transition={{ duration: 0.5 }}>
       {/* w-64 p-4 bg-neutral-800 rounded-lg shadow flex flex-col gap-4 */}
@@ -39,9 +45,9 @@ const ConversationCard: React.FC<ConversationCardProps> = ({ conversation, onUpd
         <ConversationCardHeader conversation={conversation} onDelete={onDelete} />
         <CardContent className="flex flex-grow flex-col px-4 pb-4 pt-0">
           {conversation.summarized ? (
-            <SummarizedContent conversation={conversation} />
+            <SummarizedContent conversation={conversation} onViewTranscript={handleOnViewTranscript} />
           ) : (
-            <UnsummarizedContent transcript={conversation.transcript} onSummarize={handleSummarize} />
+            <UnsummarizedContent transcript={conversation.transcript} onViewTranscript={handleOnViewTranscript} />
           )}
         </CardContent>
         <CardFooter className="px-4 pb-4 pt-0">
@@ -101,9 +107,10 @@ const ConversationCardHeader: React.FC<{ conversation: ConversationData; onDelet
         <div className="flex gap-[5px]">
           <button
             onClick={handleCopyTranscript}
-            className="relative h-5 w-5 text-white/60 transition-colors duration-200 hover:text-white"
+            className = "text-foreground transition-colors duration-200 flex items-center justify-center hover:text-brand"
+            // className="relative h-5 w-5 text-white/60 transition-colors duration-200 hover:text-white"
           >
-            <ClipboardIcon width={20} height={20} />
+            <ClipboardIcon width={24} height={24} />
             {copyState !== 'idle' && (
               <span className="absolute -top-8 left-1/2 -translate-x-1/2 transform rounded bg-neutral-700 px-2 py-1 text-xs text-white shadow-md">
                 Copied
@@ -112,25 +119,27 @@ const ConversationCardHeader: React.FC<{ conversation: ConversationData; onDelet
           </button>
           <button
             onClick={() => onDelete(conversation.id)}
-            className="h-5 w-5 text-white/60 transition-colors duration-200 hover:text-white"
+            className="text-foreground transition-colors duration-200 flex items-center justify-center hover:text-destructive"
           >
-            <TrashIcon width={20} height={20} />
+            <TrashIcon width={24} height={24} className='group-hover:text-destructive' />
           </button>
         </div>
       </div>
     </div>
   )
 }
+interface UnsummarizedContentProps {
+  transcript: string;
+  onViewTranscript: () => void;
+}
 
-const UnsummarizedContent: React.FC<{ transcript: string }> = ({ transcript }) => {
+const UnsummarizedContent: React.FC<UnsummarizedContentProps> = ({ transcript, onViewTranscript }) => {
   const scrollRef = useRef<HTMLDivElement>(null)
   const { showTopGradient, showBottomGradient } = useScrollGradient(scrollRef)
 
   return (
     <div className="relative space-y-4">
-      {' '}
-      {/* Add relative positioning */}
-      <div className="relative h-64">
+      <div className="relative h-[275px]">
         {showTopGradient && (
           <div className="pointer-events-none absolute inset-x-0 top-0 z-10 h-8 bg-gradient-to-b from-background-100 to-transparent" />
         )}
@@ -140,75 +149,76 @@ const UnsummarizedContent: React.FC<{ transcript: string }> = ({ transcript }) =
         <div ref={scrollRef} className="scrollbar-hide h-full overflow-y-auto p-0">
           <p className="select-text pb-8 text-[15px] leading-normal text-foreground/80">
             {' '}
-            {/* Add bottom padding */}
             {transcript}
           </p>
         </div>
       </div>
-      <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center">
-        <Button
-          onClick={() => {}}
-          variant="ghost"
-          className="w-full items-center justify-center rounded-lg bg-background/20 p-2 text-[15px] font-semibold text-white backdrop-blur-sm transition-colors duration-200 hover:text-white hover:bg-background/30 hover:backdrop-blur-sm"
-        >
-          View Conversation
-          <ChevronRightIcon width={24} height={24} className="ml-2" />
-        </Button>
-      </div>
+      <ViewTranscriptButton onViewTranscript={onViewTranscript} />
     </div>
   )
 }
 
-const SummarizedContent: React.FC<{ conversation: ConversationData }> = ({ conversation }) => (
-  <>
-    <div className="mb-2">
-      <h3 className="font-semibold">Topic:</h3>
-      <p className="text-sm">{conversation.topic}</p>
+interface SummarizedContentProps {
+  conversation: ConversationData;
+  onViewTranscript: () => void;
+}
+
+const SummarizedContent: React.FC<SummarizedContentProps> = ({ conversation, onViewTranscript }) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const { showTopGradient, showBottomGradient } = useScrollGradient(scrollRef);
+
+  return (
+    <div className="relative space-y-4">
+      <Badge className="mb-2">
+        Summarized
+      </Badge>
+      <div className="relative h-[210px]">
+        {showTopGradient && (
+          <div className="pointer-events-none absolute inset-x-0 top-0 z-10 h-8 bg-gradient-to-b from-background-100 to-transparent" />
+        )}
+        {showBottomGradient && (
+          <div className="pointer-events-none absolute inset-x-0 bottom-0 z-100 h-24 bg-gradient-to-t from-background-100 to-transparent" />
+        )}
+        <div ref={scrollRef} className="scrollbar-hide h-full overflow-y-auto p-0">
+          <h3 className="text-sm font-semibold text-white/60 mb-1">Topic:</h3>
+          <p className="text-sm text-white mb-4">{conversation.topic}</p>
+          <h3 className="text-sm font-semibold text-white/60 mb-1">Summary:</h3>
+          <p className="select-text text-[15px] leading-normal text-white">
+            {conversation.summary}
+          </p>
+        </div>
+      </div>
+      <div className="mt-4">
+        <Button
+          onClick={onViewTranscript}
+          variant="ghost"
+          className="w-full justify-between items-center rounded-lg bg-background/20 p-2 text-[15px] font-semibold text-white backdrop-blur-sm transition-colors duration-200 hover:text-white hover:bg-background/30 hover:backdrop-blur-sm"
+        >
+          View Conversation
+          <ArrowRightIcon className="ml-0 h-5 w-5" />
+        </Button>
+      </div>
     </div>
-    <div className="mb-2">
-      <h3 className="font-semibold">Summary:</h3>
-      <p className="text-sm">{conversation.summary}</p>
-    </div>
+  );
+};
+
+interface ViewTranscriptButtonProps {
+  onViewTranscript: () => void;
+}
+
+const ViewTranscriptButton: React.FC<ViewTranscriptButtonProps> = ({ onViewTranscript }) => (
+  <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center">
     <Button
-      onClick={() => {}} // This will be implemented later
-      variant="outline"
-      className="mt-2 w-full justify-between"
+      onClick={onViewTranscript}
+      variant="ghost"
+      className="w-full justify-between items-center rounded-lg bg-background/20 p-2 text-[15px] font-semibold text-white backdrop-blur-sm transition-colors duration-200 hover:text-white hover:bg-background/30 hover:backdrop-blur-sm"
     >
       View Conversation
-      <ChevronRightIcon className="ml-2 h-4 w-4" />
+      <ArrowRightIcon className="ml-0 h-5 w-5" />
     </Button>
-  </>
-)
+  </div>
+);
 
 export default ConversationCard
 
-//Header old
-// return (
-//   // flex flex-col gap-0.5
-//   // "flex justify-between items-center p-4"
-//   <div className="flex flex-col gap-0.5">
-//     <div>
-//       <h2 className="text-lg font-semibold">{relativeTime || 'Moments ago'}</h2>
-//       <p className="text-sm text-muted-foreground">{formatTimestamp(conversation.timestamp)}</p>
-//     </div>
-//     <div className="flex space-x-2">
-//       <button
-//         onClick={handleCopyTranscript}
-//         className="text-muted-foreground hover:text-brand transition-colors duration-200 relative"
-//       >
-//         <ClipboardIcon width={18} height={18} />
-//         {copyState !== 'idle' && (
-//           <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-background text-muted-foreground text-xs py-1 px-2 rounded shadow-md">
-//             Copied
-//           </span>
-//         )}
-//       </button>
-//       <button
-//         onClick={() => onDelete(conversation.id)}
-//         className="text-muted-foreground hover:text-destructive transition-colors duration-200"
-//       >
-//         <TrashIcon width={18} height={18} />
-//       </button>
-//     </div>
-//   </div>
-// );
+
