18c59b723f7bd89912835ace78d1111d3937d857
Maximilian Maksutovic <maksutovic@gmail.com>
2024-08-06 17:41:39 +0100

    Centered text on welcome screen

--- components/Onboarding/WelcomeScreen.tsx
+++ components/Onboarding/WelcomeScreen.tsx
@@ -16,7 +16,7 @@ const WelcomeScreen: React.FC<WelcomeScreenProps> = ({ onNext }) => {
   return (
     <OnboardingTemplate gradientTexts={gradientTexts}>
       <h1 className="text-3xl font-bold p-4 text-center">Transcribe meetings and calls automatically and securely</h1>
-      <p className="text-xl mt-4 mb-4 text-foreground-muted text-left font-regular leading-relaxed">
+      <p className="text-xl mt-4 mb-4 text-foreground-muted text-center font-regular leading-relaxed">
       All your data is completely private and can never leave
       your computer without your explicit permission
       </p>
